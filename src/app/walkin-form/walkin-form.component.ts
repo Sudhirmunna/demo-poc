cd90f37c9fa8301a35d8958429121d55877f4754
Sudhir <sudhirmunna007@gmail.com>
2018-06-14 20:02:25 -0400

    basic form layout

--- src/app/walkin-form/walkin-form.component.ts
+++ src/app/walkin-form/walkin-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { User } from '../common/common.interface';
 
 @Component({
   selector: 'app-walkin-form',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./walkin-form.component.css']
 })
 export class WalkinFormComponent implements OnInit {
+  public myForm: FormGroup; // our form model
 
-  constructor() { }
+  // using form builder to simplify syntax
+  constructor(private _fb: FormBuilder) {}
 
   ngOnInit() {
+    // we will initialize our form here
+    this.myForm = this._fb.group({
+      name: ['', [Validators.required, Validators.minLength(5)]],
+      addresses: this._fb.array([this.initAddress()])
+    });
+  }
+  initAddress() {
+    // initialize our address
+    return this._fb.group({
+      street: ['', Validators.required],
+      postcode: ['']
+    });
   }
 
+  addAddress() {
+    // add address to the list
+    const control = <FormArray>this.myForm.controls['addresses'];
+    control.push(this.initAddress());
+  }
+
+  removeAddress(i: number) {
+    // remove address from the list
+    const control = <FormArray>this.myForm.controls['addresses'];
+    control.removeAt(i);
+  }
+
+  save(model: User) {
+    // call API to save user
+    console.log(model);
+  }
 }
